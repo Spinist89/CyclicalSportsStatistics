832ecb552fd4778926c4e1a86aabb5b98c110b72
Владислав Бунин <gaius.capito@gmail.com>
2022-09-21 23:06:02 +0300

    Обновлена структура проекта, создан index.html для start_page

--- authreg/views.py
+++ authreg/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def authreg(request):
-    return HttpResponse('<h1>Авторизация, Регистрация</h1>')
+    return render(request, 'authreg/index.html')
