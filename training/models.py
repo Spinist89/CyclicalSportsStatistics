6428fbdadce7c6691bbcc59c00d0e5750977d0a6
alex <spinist.89@mail.ru>
2022-10-17 23:13:52 +0300

    обновление моделек, создание приложения Core

--- training/models.py
+++ training/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
 
-class Group(models.Model):
+class Training(models.Model):
     main_tasks = models.CharField(max_length=32)
     # coach = models.ManyToManyField()
     # sportsmen = models.ManyToManyField()
-    # statistics = models.IntegerField()
+    # intermediate_result = models.IntegerField()
+
+
+class Result(models.Model):
+    # coach = models.ManyToManyField()
+    # sportsmen = models.ManyToManyField()
+    # group = models.ManyToManyField()
+    # training = models.ManyToManyField()
+    updated_at = models.DateTimeField(auto_now=True)
+    average_result = models.IntegerField()
