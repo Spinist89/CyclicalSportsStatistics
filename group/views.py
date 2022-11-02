2ca36af96fa94d3f34ccbad7abd5e6abb9497624
alex <spinist.89@mail.ru>
2022-11-02 23:45:26 +0300

    Реализованна логика "создания группы".

--- group/views.py
+++ group/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
+from .forms import GroupForm
+from .models import Group, User
 
 
+@login_required
 def create_group(request):
-    return render(request, "group/create_group.html")
+    form = GroupForm(request.POST or None)
+    if form.is_valid():
+        new_group = form.save(commit=False)
+        new_group.coach = request.user
+        new_group.save()
+        return redirect("start_page:index")
+    return render(request, 'group/create_group.html', {'form': form})
 
 
 def group(request):
--- users/models.py
+++ users/models.py
@@ -16,31 +16,35 @@ class User(AbstractUser):
     user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES, default=1)
 
 
-class Coach(models.Model):
-    surname = models.CharField(max_length = 32)
-    name = models.CharField(max_length = 32)
-    patronymic = models.CharField(max_length = 32, null=True)
-    login = models.CharField(max_length = 32)
-    password = models.CharField(max_length = 32)
-    email = models.EmailField(max_length = 254)
-    # sport_type = models.ManyToManyField()
-    # sportsmen = models.ManyToManyField()
-    # group = models.ManyToManyField()
-    # training = models.ManyToManyField()
-    updated_at = models.DateTimeField(auto_now=True)
-    created_at = models.DateTimeField(auto_now_add = True)
 
-class Sportsmen(models.Model):
-    surname = models.CharField(max_length=32)
-    name = models.CharField(max_length=32)
-    patronymic = models.CharField(max_length=32, null=True)
-    login = models.CharField(max_length=32)
-    password = models.CharField(max_length=32)
-    email = models.EmailField(max_length=254)
-    # sport_type = models.ManyToManyField()
-    # group = models.ManyToManyField()
-    # training = models.ManyToManyField()
-    # coach = models.ManyToManyField()
-    updated_at = models.DateTimeField(auto_now=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+# class Coach(models.Model):
+#     surname = models.CharField(max_length = 32)
+#     name = models.CharField(max_length = 32)
+#     patronymic = models.CharField(max_length = 32, null=True)
+#     login = models.CharField(max_length = 32)
+#     password = models.CharField(max_length = 32)
+#     email = models.EmailField(max_length = 254)
+#     # sport_type = models.ManyToManyField()
+#     # sportsmen = models.ManyToManyField()
+#     # group = models.ManyToManyField()
+#     # training = models.ManyToManyField()
+#     updated_at = models.DateTimeField(auto_now=True)
+#     created_at = models.DateTimeField(auto_now_add = True)
+#
+# class Sportsmen(models.Model):
+#     surname = models.CharField(max_length=32)
+#     name = models.CharField(max_length=32)
+#     patronymic = models.CharField(max_length=32, null=True)
+#     login = models.CharField(max_length=32)
+#     password = models.CharField(max_length=32)
+#     email = models.EmailField(max_length=254)
+#     # sport_type = models.ManyToManyField()
+#     # group = models.ManyToManyField()
+#     # training = models.ManyToManyField()
+#     # coach = models.ManyToManyField()
+#     updated_at = models.DateTimeField(auto_now=True)
+#     created_at = models.DateTimeField(auto_now_add=True)
 
