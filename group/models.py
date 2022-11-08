5a1f83a477a5fb396b2046de6e928161d5c9e85c
alex <spinist.89@mail.ru>
2022-11-08 23:46:32 +0300

    Редактирование группы(не завершено)

--- group/models.py
+++ group/models.py
@@ -8,7 +8,8 @@ User = get_user_model()
 class Group(models.Model):
     title = models.CharField("Название группы", max_length=32)
     coach = models.ForeignKey(User, related_name="group", on_delete=models.SET_NULL, null=True)
-    sportsmen = models.ForeignKey(User, related_name="group1", on_delete=models.SET_NULL, null=True)
+    sportsmen = models.ManyToManyField(User, related_name="group1")
+
     # training = models.ForeignKey()
     created_at = models.DateTimeField("Вркемя создания", auto_now_add=True)
 
--- group/views.py
+++ group/views.py
@@ -6,15 +6,22 @@ from .models import Group, User
 
 
 @login_required
-def create_group(request):
+def create_group(request, user=None):
     form = GroupForm(request.POST or None)
     if form.is_valid():
         new_group = form.save(commit=False)
         new_group.coach = request.user
+        new_group.sportsmen = request.user
+
+
         new_group.save()
+        form.save_m2m()
         return redirect("start_page:index")
     return render(request, 'group/create_group.html', {'form': form})
 
 
+@login_required
 def group(request):
-    return render(request, "group/group.html")
+    groups = Group.objects.filter(coach=request.user)
+    print(groups)
+    return render(request, "group/group.html", {"groups":groups})
