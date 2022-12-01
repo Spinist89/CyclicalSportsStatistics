699ab3fce1f5ba6da4e4a1d6a27279e4878dbb70
alex <spinist.89@mail.ru>
2022-12-01 22:56:31 +0300

    Редактирование группы(ЗАВЕРШЕНО)))

--- group/views.py
+++ group/views.py
@@ -2,26 +2,26 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from .forms import GroupForm
-from .models import Group, User
+from users.models import Group, User
 
 
 @login_required
-def create_group(request, user=None):
+def create_group(request):
+    # return render(request, "group/create_group.html")
     form = GroupForm(request.POST or None)
     if form.is_valid():
         new_group = form.save(commit=False)
         new_group.coach = request.user
-        new_group.sportsmen = request.user
-
-
+        new_group.title = f"{request.user.username}_{form.cleaned_data['title']}"
         new_group.save()
-        form.save_m2m()
         return redirect("start_page:index")
     return render(request, 'group/create_group.html', {'form': form})
 
 
 @login_required
 def group(request):
-    groups = Group.objects.filter(coach=request.user)
-    print(groups)
-    return render(request, "group/group.html", {"groups":groups})
+    groups = Group.objects.all()
+    sportsmens = User.objects.filter(group=request.user.group).all()
+    context = {"groups":groups,
+               "sportsmens":sportsmens,}
+    return render(request, "group/group.html", context)
--- training/models.py
+++ training/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Group(models.Model):
     main_tasks = models.CharField(max_length=32)
+
     # coach = models.ManyToManyField()
     # sportsmen = models.ManyToManyField()
     # statistics = models.IntegerField()
